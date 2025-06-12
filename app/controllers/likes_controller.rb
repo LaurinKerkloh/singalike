260e95ca3a5e0cf14d2527e8e9e14537740c7c7d
Laurin Kerkloh <laurin@kerk-loh.de>
2025-06-12 16:55:36 +0200

    favicons

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,18 +1,13 @@
 class LikesController < ApplicationController
   allow_unauthenticated_access
   def create
-    @like = Like.new(like_params)
-
-    if @like.save
-      redirect_to root_path, notice: "Like was successfully created."
-    else
-      redirect_to root_path, alert: "Error."
-    end
+    @like = Like.create!(like_params)
+    redirect_to root_path
   end
 
   def destroy
     Like.find(params.expect(:id)).destroy!
-    redirect_to root_path, status: :see_other, notice: "Like was successfully destroyed."
+    redirect_to root_path
   end
 
   private
