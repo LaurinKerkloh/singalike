b8e75713d60d49072423df3a7b630bc24c7774de
Laurin Kerkloh <laurin@kerk-loh.de>
2025-06-22 13:35:43 +0200

    dark theme

---
+++ app/javascript/theme_init.js
@@ -0,0 +1,6 @@
+const theme = localStorage.getItem("theme");
+const prefersDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+if (theme === "dark" || (!theme && prefersDark)) {
+    document.documentElement.classList.add("dark");
+}
