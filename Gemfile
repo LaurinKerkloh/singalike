a3b806c136756d41c3b1a3a788f47239edb6da9f
Laurin Kerkloh <laurin@kerk-loh.de>
2026-05-24 09:34:50 +0200

    update

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source "https://rubygems.org"
 
 # Bundle edge Rails instead: gem "rails", github: "rails/rails", branch: "main"
-gem "rails", "~> 8.0.2"
+gem "rails", "~> 8.1.0"
 # The modern asset pipeline for Rails [https://github.com/rails/propshaft]
 gem "propshaft"
 # Use sqlite3 as the database for Active Record
@@ -58,8 +58,6 @@ end
 group :development do
   # Use console on exceptions pages [https://github.com/rails/web-console]
   gem "web-console"
-  gem "erb-formatter", require: false, git: "https://github.com/pelletencate/erb-formatter", ref: "99601be"
-  gem "erb_lint", require: false
 
   gem "ruby-lsp", require: false
   gem "ruby-lsp-rails", require: false
--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_06_19_072825) do
+ActiveRecord::Schema[8.1].define(version: 2025_06_19_072825) do
   create_table "likes", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "identifier"
     t.integer "song_id", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["identifier", "song_id"], name: "index_likes_on_identifier_and_song_id", unique: true
     t.index ["song_id"], name: "index_likes_on_song_id"
   end
 
   create_table "sessions", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.string "ip_address"
-    t.string "user_agent"
     t.datetime "created_at", null: false
+    t.string "ip_address"
     t.datetime "updated_at", null: false
+    t.string "user_agent"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_sessions_on_user_id"
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string "title"
     t.string "artist"
-    t.text "lyrics"
     t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean "playing", default: false, null: false
     t.datetime "last_played"
+    t.text "lyrics"
+    t.boolean "playing", default: false, null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
     t.string "email_address", null: false
     t.string "password_digest", null: false
-    t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
